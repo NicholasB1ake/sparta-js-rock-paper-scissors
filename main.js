3adde32c67339ca81626871e5737d89003d49ba8
nick <nicholasb1ake@hotmail.com>
2017-10-31 21:16:30 +0000

    computer picks2

--- main.js
+++ main.js
@@ -2,6 +2,5 @@ alert ('Lets play rock paper scissors!!');
 var valuePick = prompt('Pick rock, paper or scissors');
 
 const computerChoice = ['rock', 'paper', 'scissors']
-  (Math.floor(Math.random() * computerChoice.length))
 
-if ()
+console.log  (Math.floor(Math.random() * computerChoice.length))
