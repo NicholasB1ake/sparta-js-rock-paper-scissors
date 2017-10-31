a8ac7aad286aea613bc112dab934d4fb7262972d
nick <nicholasb1ake@hotmail.com>
2017-10-31 23:30:23 +0000

    somewhat compares rock

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
 alert ('Lets play rock paper scissors!!');
 var valuePick = prompt('Pick rock, paper or scissors');
 
-const computerChoice = ['rock', 'paper', 'scissors']
+var computerChoices = ['rock','paper', 'scissors']
+var computerChoice = Math.floor(Math.random() * computerChoices.length)
+  console.log(computerChoice);
 
-console.log  (Math.floor(Math.random() * computerChoice.length))
+if (valuePick === computerChoices[computerChoice]){
+    alert ('draw');
+
+} else if (valuePick === 'rock' && computerChoices[1]) {
+  alert('Computer picks paper! Computer Wins!')
+
+} else if (valuePick === 'rock' && computerChoices[2]){
+  alert(' Computer picks scissors You Win!')
+}
+
+// if (valuePick === 'paper' && computerChoices[0]){
+//     alert('Computer picks rock, computer wins!!')
+//   }else if (valuePick === 'paper' && computerChoices[2]) {
+//     console.log('Computer winspap');
+//
+//   }
