9b4ea2a1ba64402607f1789307db2e1aba55609c
nick <nicholasb1ake@hotmail.com>
2017-10-31 20:48:18 +0000

    Init commit

---
+++ main.js
@@ -0,0 +1,4 @@
+alert ('Lets play rock paper scissors!!');
+var valuePick = prompt('Pick rock, paper or scissors');
+
+const computerChoice = ['rock', 'paper', 'scissors']
---
+++ syntax.js
@@ -0,0 +1,5 @@
+const takeOrder = (numberOne, numberTwo) => {
+  const addition = (numberOne + numberTwo)
+  console.log(addition)
+}
+takeOrder(1,2);
